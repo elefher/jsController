5092821bb02db6f78c3037efdbe491536a449af4
elefher <elefher@debian.debian>
2013-05-26 18:37:54 +0300

    this is the first commit about JsController

---
+++ ajax.php
@@ -0,0 +1,9 @@
+<?
+
+/*if (isset($_REQUEST["id"])) {
+    echo "[{ id : 1, name : 'item 1' }]";
+} else {*/
+    //echo "[{ id : 1, name : 'item 1' }, { id : 2, name : 'item 2' }]";
+    echo "[{type:'text', name:'username'}, {type:'textarea',name:'comments'}]";
+//}
+?>
